13f961279d9a387eeda2793e80bb0abbda1a23d5
Franco Miguez <franco.miguez98@gmail.com>
2023-03-07 12:37:04 -0300

    End task 2

--- assets/scripts/carousel.js
+++
@@ -1,9 +0,0 @@
-export function createImageCarousel(events){
-    const arrayImage = document.createDocumentFragment
-    if(events.length > 4){
-        for(let i = 0; i > 6; i++){
-            
-        }
-    }else{
-    }
-}
